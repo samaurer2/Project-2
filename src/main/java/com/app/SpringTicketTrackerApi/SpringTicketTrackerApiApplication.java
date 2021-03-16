5c2bfe7feb0432f5d69a59c3e66d16239fadd3d6
samaurer2 <43677441+samaurer2@users.noreply.github.com>
2021-03-16 09:24:45 -0500

    Initial commit

---
+++ src/main/java/com/app/SpringTicketTrackerApi/SpringTicketTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.SpringTicketTrackerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTicketTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTicketTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/SpringTicketTrackerApi/SpringTicketTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SpringTicketTrackerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTicketTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
