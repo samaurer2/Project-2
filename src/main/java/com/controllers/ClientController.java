244e1389cc8db30b39101965956a554b32bd129c
samaurer2 <43677441+samaurer2@users.noreply.github.com>
2021-03-17 11:17:59 -0500

    Testing for SonarCloud analysis

--- src/main/java/com/controllers/ClientController.java
+++ src/main/java/com/controllers/ClientController.java
@@ -1,2 +1,4 @@
-package com.controllers;public class ClientController {
+package com.controllers;
+
+public class ClientController {
 }
--- src/main/java/com/entities/Ticket.java
+++ src/main/java/com/entities/Ticket.java
@@ -1,4 +1,23 @@
 package com.entities;
 
 public class Ticket {
+    private int ticketId;
+
+    public Ticket() {
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "ticketId=" + ticketId +
+                '}';
+    }
 }
