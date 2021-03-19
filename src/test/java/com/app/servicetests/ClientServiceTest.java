60346737ac052f71e812bf65c24545bf8eecb20c
Aristotle <a.debride.2016@gmail.com>
2021-03-19 11:03:08 -0400

    Initialize mockito in ClientServiceTest.

--- src/test/java/com/app/servicetests/ClientServiceTest.java
+++ src/test/java/com/app/servicetests/ClientServiceTest.java
@@ -8,12 +8,15 @@ import com.services.ClientServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ClientServiceTest {
 
 
