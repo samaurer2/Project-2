80736459057afc8b303845f3194763f25edde1ca
samaurer2 <43677441+samaurer2@users.noreply.github.com>
2021-03-28 23:54:23 -0500

    New Branch

--- src/main/java/com/controllers/TechnicianController.java
+++ src/main/java/com/controllers/TechnicianController.java
@@ -143,6 +143,7 @@ public class TechnicianController {
     @PutMapping("/tech/ticket")
     @ResponseBody
     public ResponseEntity<Object> modifyTicketStatus(@RequestBody Ticket ticket, @RequestHeader("Authorization") String jwt, @RequestParam(value = "closed", required = false) boolean closed) {
+        System.out.println(ticket);
         DecodedJWT decodedJWT = JwtUtil.isValidJWT(jwt);
         System.out.println(ticket);
         try {
